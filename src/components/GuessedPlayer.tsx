1b775cd470c7a3c068d3e8adedb0a5a6318c603f
Nicolas Moyano <nicomoyano01@gmail.com>
2023-02-16 14:49:29 -0300

    Guessed player use new items

--- src/components/GuessedPlayer.tsx
+++ src/components/GuessedPlayer.tsx
@@ -1,7 +1,10 @@
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import React, { useContext } from 'react';
 import { Player } from '../types/Player';
 import { gameContext } from '../store/gameContext';
+import ImageItem from './PlayerItems/ImageItem';
+import AgeItem from './PlayerItems/AgeItem';
+import RegionItem from './PlayerItems/RegionItem';
 
 type Props = {
   player: Player;
@@ -13,88 +16,39 @@ const GuessedPlayer = ({ player }: Props) => {
 
   return (
     <View style={styles.container}>
-      <Image
-        source={player.image}
-        style={[
-          styles.image,
-          player.id === playerToGuess.id && styles.correctImage,
-        ]}
-      />
-      <View>
-        <Image
-          source={player.country.regionImage}
-          style={[
-            styles.image,
-            player.country.region === playerToGuess.country.region &&
-              styles.correctImage,
-          ]}
+      <Text style={styles.playerName}>{player.name}</Text>
+      <View style={styles.itemsContainer}>
+        <RegionItem
+          name={player.country.region}
+          image={player.country.regionImage}
+          isCorrect={player.country.region === playerToGuess.country.region}
         />
-        <Text
-          style={{
-            position: 'absolute',
-            height: 50,
-            width: 50,
-            textAlign: 'center',
-            lineHeight: 50,
-            color: 'white',
-            fontWeight: '900',
-            fontSize: 16,
-            borderRadius: 25,
-            backgroundColor: 'rgba(0, 0, 0, 0.3)',
-            overflow: 'hidden',
-          }}
-        >
-          {player.country.region.slice(0, 2).toUpperCase()}
-        </Text>
+        <ImageItem
+          image={player.country.image}
+          isCorrect={player.country.id === playerToGuess.country.id}
+        />
+        <ImageItem
+          image={player.club.league.image}
+          isCorrect={player.club.league.id === playerToGuess.club.league.id}
+        />
+        <ImageItem
+          image={player.club.image}
+          isCorrect={player.club.id === playerToGuess.club.id}
+        />
+        <ImageItem
+          image={player.position.image}
+          isCorrect={player.position.general === playerToGuess.position.general}
+        />
+        <ImageItem
+          image={
+            player.foot === 'Right'
+              ? require('../images/feet/foot_right.png')
+              : require('../images/feet/foot_left.png')
+          }
+          isCorrect={player.foot === playerToGuess.foot}
+        />
+        <AgeItem playerAge={player.age} playerToGuessAge={playerToGuess.age} />
       </View>
-      <Image
-        source={player.country.image}
-        style={[
-          styles.image,
-          player.country.id === playerToGuess.country.id && styles.correctImage,
-        ]}
-      />
-      <Image
-        source={player.club.league.image}
-        style={[
-          styles.image,
-          player.club.league.id === playerToGuess.club.league.id &&
-            styles.correctImage,
-        ]}
-      />
-      <Image
-        source={player.club.image}
-        style={[
-          styles.image,
-          player.club.id === playerToGuess.club.id && styles.correctImage,
-        ]}
-      />
-      <Image
-        source={player.position.image}
-        style={[
-          styles.image,
-          player.position.general === playerToGuess.position.general &&
-            styles.correctImage,
-        ]}
-      />
-      <Image
-        source={require('../images/foot.png')}
-        style={[
-          styles.image,
-          player.foot === 'Right' && { transform: [{ scaleX: -1 }] },
-          player.foot === playerToGuess.foot && styles.correctImage,
-        ]}
-      />
-      <Text
-        style={[
-          styles.text,
-          player.age === playerToGuess.age && styles.correctText,
-        ]}
-      >
-        {player.age}
-        {player.age < playerToGuess.age && '↑'}
-        {player.age > playerToGuess.age && '↓'}
-      </Text>
     </View>
   );
 };
@@ -104,29 +58,19 @@ const styles = StyleSheet.create({
     backgroundColor: 'lightgray',
     borderRadius: 10,
     marginVertical: 10,
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    paddingVertical: 10,
     alignItems: 'center',
+  },
+  playerName: {
+    textTransform: 'uppercase',
+    fontWeight: '700',
+  },
+  itemsContainer: {
+    width: '100%',
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
     flexWrap: 'wrap',
-  },
-  text: {
-    fontSize: 20,
-    color: 'red',
-    width: 50,
-    textAlign: 'center',
-  },
-  correctText: {
-    color: 'green',
-  },
-  image: {
-    width: 50,
-    height: 50,
-    resizeMode: 'contain',
-    backgroundColor: 'red',
-  },
-  correctImage: {
-    backgroundColor: 'green',
   },
 });
 
