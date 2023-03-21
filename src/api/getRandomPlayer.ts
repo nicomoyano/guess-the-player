f6a569f9df83bdd40feaa4db0030a4748f0b31cc
Nicolas Moyano <nicomoyano01@gmail.com>
2023-03-21 12:08:07 -0300

    Fixed random player function

--- src/api/getRandomPlayer.ts
+++ src/api/getRandomPlayer.ts
@@ -4,13 +4,12 @@ import { getPlayer } from './utils/getPlayer';
 
 export const getRandomPlayer = (): Player => {
   const filteredRawPlayers = rawPlayers.filter(
-    (rawPlayer) => rawPlayer.overall >= 85
+    (rawPlayer) => rawPlayer.overall >= 80
   );
 
   const rawPlayer =
     filteredRawPlayers[Math.floor(Math.random() * filteredRawPlayers.length)];
   const player = getPlayer(rawPlayer);
-  console.log(player.name);
 
   return player;
 };
